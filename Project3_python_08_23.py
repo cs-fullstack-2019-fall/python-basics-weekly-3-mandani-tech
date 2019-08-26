3bcfdfc65dc1294459e63128318deabc2cf4d4c5
Kevin Yancy CodeCrew <40698421+Kevin-CodeCrew@users.noreply.github.com>
2019-08-25 20:35:11 -0500

    Graded Weekly 3 Python - See comments in README and/or source code

--- Project3_python_08_23.py
+++ Project3_python_08_23.py
@@ -1,4 +1,9 @@
 # python-basics-weekly-3
+
+### VERY nice work! Great comments, easy to read and follow. Well done!
+###
+### Showing the current balance as part of a deposit or with draw was a *really* nice touch!
+
 ## Create a bank program. # Ask account holder for account name and pin
 print("Welcome to Bank of MandaniTech")
 account_Name= input("Please enter the name on this account\n:  ")
@@ -7,11 +12,13 @@ account_pin= input("Please enter your account pin\n:")
 #User enters an option from the given menu
 user_Balance=0
 mainMenu=0
-print(f'Enter from an option below:\n'
-                        f'1) Check balance\n'
-                        f'2) Add money to account\n'
-                        f'3) Withdraw money from account\n'
-                        f'4) Quit\n')
+### This menu isn't needed and makes 2 menus print when you first launch the program.
+### As soon as first iteration of loop starts, you are going to display menu anyway.
+# print(f'Enter from an option below:\n'
+#                         f'1) Check balance\n'
+#                         f'2) Add money to account\n'
+#                         f'3) Withdraw money from account\n'
+#                         f'4) Quit\n')
 
 while(mainMenu !='4'):
     mainMenu =input(f'Hello,What will you like to do?\n'
